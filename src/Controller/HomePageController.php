09035dcb1cee2dd7b188e335acf5d195313ae371
Brandon Tapiba <brandontapiba@gmail.com>
2024-05-01 22:32:07 +0100

    added some bootstrap to the registration forms

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -15,4 +15,41 @@ class HomePageController extends AbstractController
             'controller_name' => 'HomePageController',
         ]);
     }
+
+    #[Route('/about', name: 'app_about_page')]
+    public function about(){
+        return $this->render('home_page/about.html.twig', [
+            'controller_name' => 'HomePageController',
+        ]);
+    }
+
+    #[Route('/contact_us', name: 'app_contact_us_page')]
+    public function contactUs(){
+        return $this->render('home_page/contact_us.html.twig', [
+            'controller_name' => 'HomePageController',
+        ]);
+    }
+
+    #[Route('/documentation', name: 'app_documentation_page')]
+    public function documentation(){
+        return $this->render('home_page/documentation.html.twig', [
+            'controller_name' => 'HomePageController',
+        ]);
+    }
+
+    #[Route('/login', name: 'app_login_page')]
+    public function login(){
+        return $this->render('home_page/index.html.twig', [
+            'controller_name' => 'HomePageController',
+        ]);
+    }
+
+    #[Route('/register', name: 'app_register_page')]
+    public function register(){
+        return $this->render('home_page/index.html.twig', [
+            'controller_name' => 'HomePageController',
+        ]);
+    }
+
 }
+
